c9ec34d4ce28d73e6f637efafa478434eedc5d17
PrachiDhiman5 <prachidhiman362@gmail.com>
2026-06-03 15:19:10 +0530

    Updating base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://inventory-order-api-oecm.onrender.com/api",
 });
 
 // Interceptor to automatically attach JWT token to all requests
